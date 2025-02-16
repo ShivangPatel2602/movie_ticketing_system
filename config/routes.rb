94bacd0c1df6f8cfb926c4cd85f18d9868bc5d7b
Shivang Patel <shivang.patel02062002@gmail.com>
2025-02-16 13:03:01 -0500

    modified routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,21 @@
 Rails.application.routes.draw do
-  root "home#index"
-
-  # Users & Authentication
-  resources :users
-  resources :sessions, only: [ :new, :create, :destroy ]
-  get "signup", to: "users#new", as: "signup"
-  get "login", to: "sessions#new", as: "login"
-  get "logout", to: "sessions#destroy", as: "logout"
-
-  # Admin Authentication
-  resources :admins, only: [ :edit, :update ]
-  resources :admin_sessions, only: [ :new, :create, :destroy ]
-  get "admin_login", to: "admin_sessions#new", as: "admin_login"
-  get "admin_logout", to: "admin_sessions#destroy", as: "admin_logout"
-
-  # Movies & Shows (Nesting for correct path helpers)
-  resources :movies do
-    resources :shows, only: [ :index, :show, :new, :create ] # Shows must belong to a Movie
-  end
-
-  resources :shows, except: [ :index, :show, :new, :create ] # Admin Show Management
-
-  # Tickets (Users Only)
   resources :tickets
+  resources :shows do
+    resources :tickets, only: [:new, :create]  # or include other actions as needed
+  end
+  resources :movies do
+    resources :shows do
+      resources :tickets, only: [:new, :create, :index]
+    end
+  end
+  resources :admins, only: [:edit, :update]
+  root 'home#index'
+  resources :users
+  resources :admin_sessions, only: [:new, :create, :destroy]
+  resources :sessions, only: [:new, :create, :destroy]
+  get 'signup', to: "users#new", as: 'signup'
+  get 'login', to: "sessions#new", as: 'login'
+  get 'logout', to: "sessions#destroy", as: 'logout'
+  get 'admin_login', to: "admin_sessions#new", as: 'admin_login'
+  get 'admin_logout', to: "admin_sessions#destroy", as: 'admin_logout'
 end
